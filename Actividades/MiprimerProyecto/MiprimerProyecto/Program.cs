519f68737877d197269c23c5af54344dfcf39009
Rosario1218IB <61705977+Rosario1218IB@users.noreply.github.com>
2020-03-02 15:47:37 -0500

    Prueba CVS

--- Actividades/MiprimerProyecto/MiprimerProyecto/Program.cs
+++ Actividades/MiprimerProyecto/MiprimerProyecto/Program.cs
@@ -7,6 +7,7 @@
         {
             Console.WriteLine("Hola Mundo");
             Console.WriteLine("Este es mi programa en C");
+            Console.WriteLine("Esta es una prueba de CVS");
             Console.Read();
         }
     }
