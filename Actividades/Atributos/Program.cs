41474dba485991311fd34aa65bcd69c825585aab
Maria_del_Rosario_Zuñiga <charoth85@gmail.com>
2020-03-02 12:03:03 -0500

    Guardar ejercicio atributos

---
+++ Actividades/Atributos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+static void Main(string[] args)
+{
+    Aprendiz apSena = new Aprendiz();
+    apSena.NombresAprendiz = " Maria del Rosario ";
+    apSena.ApellidosAprendiz = " Zuñiga Sampablo ";
+    apSena.Direccion= " Calle 19sur";
+    apSena.MunicipioResidencia = " Pitalito ";
+     
+    Saludo(apSena);
+}
+private static void Saludo(Aprendiz ApSena)
+    {
+        Console.WriteLine(" Bienvenido Sr(a): "+ ApSena.NombresAprendiz +
+                          " Su apellido es: " + ApSena.ApellidosAprendiz +
+                          " Su direccion es: " + ApSena.Direccion +                
+                          " Su ciudad de residencia es: " + ApSena.MunicipioResidencia);
+        Console.ReadLine();
+    }
+}
+
